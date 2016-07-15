0a8cf270c7890362b8ff6024d88a491c20e0e620
Your Name <yonnyquiceno@gmail.com>
2016-07-14 20:53:10 -0500

    Adds initial routing configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :users do
+    resources :vehicles
+  end
+
+  resources :locations do
+    resources :slots
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
