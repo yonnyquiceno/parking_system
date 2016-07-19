5b3cfd4f976a030e8d620c78d98a542c5acc1e36
Your Name <yonnyquiceno@gmail.com>
2016-07-18 20:57:27 -0500

    rendering show view from users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  respond_to :html, :json
   protect_from_forgery with: :exception
+  before_filter :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,
+                                                       :last_name,
+                                                       :email,
+                                                       :password,
+                                                       :birthdate])
+  end
+  protected :configure_permitted_parameters
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'users#show'
   devise_for :users
   resources :users do
     resources :vehicles
